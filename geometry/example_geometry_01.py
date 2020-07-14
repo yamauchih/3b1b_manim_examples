00998dea0b629e1990f60531bd2d511811c7bbf2
Hitoshi Yamauchi <hitoshi@sr.eu>
2020-07-14 12:19:14 +0200

    Add comparison between transforma a line and rotate a line

--- geometry/example_geometry_01.py
+++ geometry/example_geometry_01.py
@@ -11,16 +11,17 @@
 #  * Theorem of Beethoven (https://www.youtube.com/channel/UCxiWCEdx7aY88bSEUgLOC6A)
 #
 # Preview resolution rendering
-#   python3 -m manim example_geometry_01.py Example_Geometry_01_01 --resolution 360,640 -pl
+#   python3 -m manim example_geometry_01.py ExampleGeometry_01_01 --resolution 360,640 -pl
 # Full resolution rendering
-#   python3 -m manim example_geometry_01.py Example_Geometry_01_01 --resolution 720,1280 -p --high_quality
+#   python3 -m manim example_geometry_01.py ExampleGeometry_01_01 --resolution 720,1280 --high_quality
 #
+# -i as gif
 
 from manimlib.imports import *
 import os, copy
 import pyclbr
 
-class Example_Geometry_01_01(Scene):
+class ExampleGeometry_01_01(Scene):
     """
     Example Geometry_01 01
     A line moves between x = x_0, x_1
@@ -44,3 +45,32 @@ class Example_Geometry_01_01(Scene):
         # Note: set_start_and_end_attrs doesn't change (no point generation?)
         self.play(ApplyMethod(line_1.put_start_and_end_on, start_1, end_1))
         self.wait(3)
+
+
+class ExampleGeometry_02_01(Scene):
+    """
+    Example Geometry_02 01
+
+    Comparison between transforming a line and rotating a line
+    """
+    CONFIG={
+    }
+
+    def construct(self):
+
+        x_0 = -1.5
+        x_1 =  1.5
+        start_0 = ORIGIN + x_0 * RIGHT
+        end_0   = ORIGIN + x_1 * RIGHT
+        line_1 = Line(start_0, end_0)
+        line_2 = Line(start_0, end_0)
+        self.play(ShowCreation(line_1))
+
+        # rotate line animation by set angle (default: linear interpolation)
+        self.play(ApplyMethod(line_1.set_angle, 1.5 * PI/2))
+        self.wait(1)
+
+        # rotate line animation by Rotate
+        self.play(FadeOut(line_1), FadeIn(line_2))
+        self.play(Rotate(line_2, angle=1.5 * PI/2, about_point=line_2.get_start()))
+        self.wait(5)
